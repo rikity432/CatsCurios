d47704fc158e92791c932eb1258075d9fd24be02
rikity432 <andre.shaw.wow@gmail.com>
2026-02-12 16:55:34 +0000

    Fix DB env config; add blog models/migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
-# Create your models here.
+
+# Mood choices for the cat
+MOOD_CHOICES = [
+    ('happy', 'Happy'),
+    ('sleepy', 'Sleepy'),
+    ('angry', 'Angry'),
+    ('curious', 'Curious'),
+    ('chaotic', 'Chaotic'),
+]
+
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    mood = models.CharField(max_length=20, choices=MOOD_CHOICES, default='curious')
+    image = CloudinaryField('image', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.post}"
--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'thecatinthehat1420'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CLOUDINARY_STORAGE = {
-    'CLOUDINARY_URL': os.environ.get('cloudinary://722475841199578:Tt3R0nR2oM4WK4wGKZk4W6C1pcI@dfgqpqmw4')
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -87,16 +86,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('postgresql://neondb_owner:npg_HGNVFmakL8Y0@ep-hidden-math-agi01q63.c-2.eu-central-1.aws.neon.tech/same_plot_dug_672263'))
-}
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
